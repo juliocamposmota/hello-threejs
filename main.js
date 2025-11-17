1620aa5e7797c3e71b9136a30e66246e0888a803
julio-findme <julio@findme.id>
2025-11-17 10:48:47 -0300

    fix: adjust scroll y and x

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 import * as THREE from 'three';
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(75, (window.innerWidth / window.innerHeight), 0.1, 1000);
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
+renderer.domElement.style.width = "100vw";
+renderer.domElement.style.height = "100vh";
 
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
